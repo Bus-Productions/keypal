b146ef96579584325c57ba06e74e5a3311931ec8
Billy <william@busproductions.com>
2013-05-18 14:22:00 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keypal::Application.config.secret_token = 'f45a3ed08fef00f066d6acf3c0bb9f46d2302337f9a566a1d151b519897b653e4bfc359ae159a2c119f04545d4c82a407aa185074cfca54de0815108cbd1a060'
