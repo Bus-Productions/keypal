91003ea37775cf6fce9c54feb70adc2b91d8be74
Billy <william@busproductions.com>
2013-05-18 22:25:54 -0500

    parsing

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -27,7 +27,9 @@ class RequestsController < ApplicationController
       @body = params[:Body]
       words = @body.split(/\W+/)
 
-      if (words[0]).gsub!(/\s/,'') == 'info'
+      first_word = words[0].gsub(" ", "")
+
+      if first_word == 'info'
 
         message = @client.account.sms.messages.create(:body => "THIS IS THE HELP SCREEN",
           :to => user.number,     # Replace with your phone number
