a78491eef4f840db3a44da8de72c98cca228bc46
Billy <william@busproductions.com>
2013-05-25 10:52:16 -0500

    stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  protect
+  protected
   def get_user_info_by_number (number)
   	encrypted_number = Digest::SHA1.hexdigest("#{@number}sm1thsbeach_21_wls")
   	@user = User.find_by_number(encrypted_number)
