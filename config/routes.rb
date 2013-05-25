ef42264ab70bdefedfb4c623ae09c0a42508eb0f
Billy <william@busproductions.com>
2013-05-25 12:17:30 -0500

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Keypal::Application.routes.draw do
 
   match 'charges/upgrade' => 'charges#upgrade', :as => "upgrade"
 
-  root :to => "users#index"
+  root :to => "users#index", :as => "users"
 
 
   resources :outbounds
